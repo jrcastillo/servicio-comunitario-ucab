88fa66fb1aad7e52e932b268f30da00cc53f9824
luis17991 <luis17991@localhost>
2014-12-20 22:09:13 -0530

    Primer Commit Luis

---
+++ SistGestionAdministrativaConsultorioMedicoChuao/SistGestionAdministrativaConsultorioMedicoChuao/Vista/VentanaPrincipal.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace SistGestionAdministrativaConsultorioMedicoChuao
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L_MenuPrincipal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.B_Pagos = new System.Windows.Forms.Button();
+            this.B_Mensajes = new System.Windows.Forms.Button();
+            this.B_Horarios = new System.Windows.Forms.Button();
+            this.B_ListaEspera = new System.Windows.Forms.Button();
+            this.B_Inscripcion = new System.Windows.Forms.Button();
+            this.B_BuscarPaciente = new System.Windows.Forms.Button();
+            this.B_Inventario = new System.Windows.Forms.Button();
+            this.B_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // L_MenuPrincipal
+            // 
+            this.L_MenuPrincipal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.L_MenuPrincipal.AutoSize = true;
+            this.L_MenuPrincipal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.L_MenuPrincipal.Font = new System.Drawing.Font("Harlow Solid Italic", 56.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_MenuPrincipal.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.L_MenuPrincipal.Location = new System.Drawing.Point(372, 9);
+            this.L_MenuPrincipal.Name = "L_MenuPrincipal";
+            this.L_MenuPrincipal.Size = new System.Drawing.Size(544, 95);
+            this.L_MenuPrincipal.TabIndex = 1;
+            this.L_MenuPrincipal.Text = "Menú Principal";
+            this.L_MenuPrincipal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "INSCRIPCIÓN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(384, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "BUSCAR PACIENTE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(678, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 24);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "LISTA DE ESPERA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1017, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 24);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "HORARIOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(86, 638);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 24);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "MENSAJES";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(382, 638);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 24);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "PAGOS/SERVICIOS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(697, 638);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "INVENTARIO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(968, 638);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 24);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "SALIR DEL SISTEMA";
+            // 
+            // B_Pagos
+            // 
+            this.B_Pagos.BackColor = System.Drawing.Color.Transparent;
+            this.B_Pagos.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.Pagos;
+            this.B_Pagos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.B_Pagos.FlatAppearance.BorderSize = 0;
+            this.B_Pagos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Pagos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Pagos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Pagos.Location = new System.Drawing.Point(351, 390);
+            this.B_Pagos.Name = "B_Pagos";
+            this.B_Pagos.Size = new System.Drawing.Size(272, 231);
+            this.B_Pagos.TabIndex = 8;
+            this.B_Pagos.UseVisualStyleBackColor = false;
+            // 
+            // B_Mensajes
+            // 
+            this.B_Mensajes.BackColor = System.Drawing.Color.Transparent;
+            this.B_Mensajes.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.Mensajes;
+            this.B_Mensajes.FlatAppearance.BorderSize = 0;
+            this.B_Mensajes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Mensajes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Mensajes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Mensajes.Location = new System.Drawing.Point(27, 401);
+            this.B_Mensajes.Name = "B_Mensajes";
+            this.B_Mensajes.Size = new System.Drawing.Size(237, 234);
+            this.B_Mensajes.TabIndex = 7;
+            this.B_Mensajes.UseVisualStyleBackColor = false;
+            // 
+            // B_Horarios
+            // 
+            this.B_Horarios.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.Horario;
+            this.B_Horarios.FlatAppearance.BorderSize = 0;
+            this.B_Horarios.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Horarios.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Horarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Horarios.Location = new System.Drawing.Point(991, 114);
+            this.B_Horarios.Name = "B_Horarios";
+            this.B_Horarios.Size = new System.Drawing.Size(244, 237);
+            this.B_Horarios.TabIndex = 6;
+            this.B_Horarios.UseVisualStyleBackColor = true;
+            // 
+            // B_ListaEspera
+            // 
+            this.B_ListaEspera.BackColor = System.Drawing.Color.Transparent;
+            this.B_ListaEspera.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.ListaEspera;
+            this.B_ListaEspera.FlatAppearance.BorderSize = 0;
+            this.B_ListaEspera.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.B_ListaEspera.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_ListaEspera.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_ListaEspera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_ListaEspera.Location = new System.Drawing.Point(657, 122);
+            this.B_ListaEspera.Name = "B_ListaEspera";
+            this.B_ListaEspera.Size = new System.Drawing.Size(245, 229);
+            this.B_ListaEspera.TabIndex = 5;
+            this.B_ListaEspera.UseVisualStyleBackColor = false;
+            // 
+            // B_Inscripcion
+            // 
+            this.B_Inscripcion.BackColor = System.Drawing.Color.Transparent;
+            this.B_Inscripcion.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.Inscripcion;
+            this.B_Inscripcion.FlatAppearance.BorderSize = 0;
+            this.B_Inscripcion.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.B_Inscripcion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Inscripcion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Inscripcion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Inscripcion.Location = new System.Drawing.Point(47, 128);
+            this.B_Inscripcion.Name = "B_Inscripcion";
+            this.B_Inscripcion.Size = new System.Drawing.Size(248, 223);
+            this.B_Inscripcion.TabIndex = 4;
+            this.B_Inscripcion.UseVisualStyleBackColor = false;
+            // 
+            // B_BuscarPaciente
+            // 
+            this.B_BuscarPaciente.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.BusquedaPaciente;
+            this.B_BuscarPaciente.FlatAppearance.BorderSize = 0;
+            this.B_BuscarPaciente.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.B_BuscarPaciente.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_BuscarPaciente.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_BuscarPaciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_BuscarPaciente.Location = new System.Drawing.Point(377, 149);
+            this.B_BuscarPaciente.Name = "B_BuscarPaciente";
+            this.B_BuscarPaciente.Size = new System.Drawing.Size(203, 202);
+            this.B_BuscarPaciente.TabIndex = 3;
+            this.B_BuscarPaciente.UseVisualStyleBackColor = true;
+            // 
+            // B_Inventario
+            // 
+            this.B_Inventario.BackColor = System.Drawing.Color.Transparent;
+            this.B_Inventario.BackgroundImage = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.Inventario;
+            this.B_Inventario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.B_Inventario.CausesValidation = false;
+            this.B_Inventario.FlatAppearance.BorderSize = 0;
+            this.B_Inventario.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.B_Inventario.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Inventario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Inventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Inventario.Location = new System.Drawing.Point(657, 415);
+            this.B_Inventario.Name = "B_Inventario";
+            this.B_Inventario.Size = new System.Drawing.Size(223, 206);
+            this.B_Inventario.TabIndex = 2;
+            this.B_Inventario.UseVisualStyleBackColor = false;
+            // 
+            // B_Salir
+            // 
+            this.B_Salir.BackColor = System.Drawing.Color.Transparent;
+            this.B_Salir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.B_Salir.CausesValidation = false;
+            this.B_Salir.FlatAppearance.BorderSize = 0;
+            this.B_Salir.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.B_Salir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.B_Salir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.B_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B_Salir.Image = global::SistGestionAdministrativaConsultorioMedicoChuao.Properties.Resources.SalirSistema;
+            this.B_Salir.Location = new System.Drawing.Point(980, 429);
+            this.B_Salir.Name = "B_Salir";
+            this.B_Salir.Size = new System.Drawing.Size(197, 190);
+            this.B_Salir.TabIndex = 0;
+            this.B_Salir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.B_Salir.UseVisualStyleBackColor = false;
+            this.B_Salir.Click += new System.EventHandler(this.B_Salir_Click);
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(1284, 681);
+            this.ControlBox = false;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.B_Pagos);
+            this.Controls.Add(this.B_Mensajes);
+            this.Controls.Add(this.B_Horarios);
+            this.Controls.Add(this.B_ListaEspera);
+            this.Controls.Add(this.B_Inscripcion);
+            this.Controls.Add(this.B_BuscarPaciente);
+            this.Controls.Add(this.B_Inventario);
+            this.Controls.Add(this.L_MenuPrincipal);
+            this.Controls.Add(this.B_Salir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "VentanaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menú principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_Salir;
+        private System.Windows.Forms.Label L_MenuPrincipal;
+        private System.Windows.Forms.Button B_Inventario;
+        private System.Windows.Forms.Button B_BuscarPaciente;
+        private System.Windows.Forms.Button B_Inscripcion;
+        private System.Windows.Forms.Button B_ListaEspera;
+        private System.Windows.Forms.Button B_Horarios;
+        private System.Windows.Forms.Button B_Mensajes;
+        private System.Windows.Forms.Button B_Pagos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SistGestionAdministrativaConsultorioMedicoChuao/SistGestionAdministrativaConsultorioMedicoChuao/Vista/VentanaPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistGestionAdministrativaConsultorioMedicoChuao
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void B_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
